c529cb6dcd6b4e4f56ef352835551833d6c8d4f8
Justin Wamalwa <justinwamalwa3@gmail.com>
2019-12-01 14:02:36 +0300

    Renamed batch to class.

--- src/app/core/student/student.module.ts
+++ src/app/core/student/student.module.ts
@@ -9,11 +9,11 @@ import { StudentService } from './student.service';
 import { HttpClientModule } from '@angular/common/http';
 import { StudentPageComponent } from './student-page/student-page.component';
 import { PaginationModule } from '../pagination/pagination.module';
-import { BatchService } from '../batch/batch.service';
+import { ClassService } from '../class/class.service';
 import { SubjectService } from '../subject/subject.service';
 import { ExamService } from '../exams/exam.service';
 import { FinanceService } from '../finance/finance.service';
-import { InstituteService } from '../institute/institute.service';
+import { HostelsService } from '../hostels/hostels.service';
 import { ToastrService } from 'src/app/toastr.service';
 import { LoaderModule } from 'src/app/loader/loader.module';
 import { Http } from 'src/app/http/http';
@@ -37,11 +37,11 @@ import { StudentListComponent } from './student-list/student-list.component';
   ],
   providers: [
     StudentService,
-    BatchService,
+    ClassService,
     SubjectService,
     ExamService,
     FinanceService,
-    InstituteService,
+    HostelsService,
     ToastrService,
     Http
   ]
