430e2baa9b03088403b1bccd99d4863b8178a72f
Justin Wamalwa <justinwamalwa3@gmail.com>
2020-01-07 14:29:27 +0300

    Functionality for viewing teacher classes added.

--- src/app/mis/teachers/view-class-teachers/view-class-teachers.component.ts
+++ src/app/mis/teachers/view-class-teachers/view-class-teachers.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { TeacherService } from '../teacher.service';
+import { ToastrService } from 'src/app/toastr.service';
 
 @Component({
   selector: 'app-view-class-teachers',
@@ -6,10 +8,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./view-class-teachers.component.scss']
 })
 export class ViewClassTeachersComponent implements OnInit {
+  teachers: any[];
+  year: number = new Date().getFullYear();
+  years: number[];
+  isLoading: boolean = false;
 
-  constructor() { }
+  constructor(
+    private teacherService: TeacherService,
+    private toastr: ToastrService
+  ) { }
 
   ngOnInit() {
+    this.getTeachers(this.year);
+    this.years = [];
+    for (let i = this.year; i >= (this.year - 4); i --) {
+      this.years.push(i);
+    }
+  }
+
+  getTeachers(year: number) {
+    this.isLoading = true;
+    this.teacherService.getClassTeachers(year)
+      .subscribe(res => {
+        this.teachers = res;
+        this.isLoading = false;
+      }, e => {
+        this.isLoading = false;
+        this.toastr.error("Failed to load class teachers.");
+      });
+  }
+
+  onYearChange(year: number) {
+    this.getTeachers(+year);
   }
 
 }
