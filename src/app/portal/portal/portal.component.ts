007d0196123dac466efcd89cd6220d6ffa90385b
Justin Wamalwa <justinwamalwa3@gmail.com>
2020-01-28 17:36:45 +0300

    Added links for module authorization

--- src/app/portal/portal/portal.component.ts
+++ src/app/portal/portal/portal.component.ts
@@ -1,6 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/auth.service';
+import { attendanceLinks } from 'src/app/mis/attendance/links';
+import { classLinks } from 'src/app/mis/classes/links';
+import { employeeLinks } from 'src/app/mis/employees/links';
+import { examLinks } from 'src/app/mis/exams/links';
+import { feeLinks } from 'src/app/mis/fees/links';
+import { hostelLinks } from 'src/app/mis/hostels/links';
+import { paymentLinks } from 'src/app/mis/payments/links';
+import { setupLinks } from 'src/app/mis/setup/links';
+import { smsLinks } from 'src/app/mis/sms/links';
+import { studentLinks } from 'src/app/mis/students/links';
+import { subjectLinks } from 'src/app/mis/subjects/links';
+import { teacherLinks } from 'src/app/mis/teachers/links';
+import { userLinks } from 'src/app/mis/users/links';
 
 @Component({
   selector: 'app-portal',
@@ -10,6 +23,19 @@ import { AuthService } from 'src/app/auth.service';
 export class PortalComponent implements OnInit {
   menuShown: boolean = false;
   user: any;
+  attendanceModule = attendanceLinks();
+  classModule = classLinks();
+  employeeModule = employeeLinks();
+  examModule = examLinks();
+  feeModule = feeLinks();
+  hostelModule = hostelLinks();
+  paymentModule = paymentLinks();
+  setupModule = setupLinks();
+  smsModule = smsLinks();
+  studentModule = studentLinks();
+  subjectModule = subjectLinks();
+  teacherModule = teacherLinks();
+  userModule = userLinks();
 
   constructor(private router: Router, private auth: AuthService) { }
 
@@ -30,6 +56,7 @@ export class PortalComponent implements OnInit {
       }
     });
     this.user = this.auth.getUserDetails();
+    this.user.type = this.user.type.toLowerCase();
   }
 
   toggleMenu() {
