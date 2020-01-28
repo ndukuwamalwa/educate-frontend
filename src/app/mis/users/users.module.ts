0f6f473624fe84e6c4afbae686cf056647f78cc7
Justin Wamalwa <justinwamalwa3@gmail.com>
2020-01-28 13:09:40 +0300

    User groups removed.

--- src/app/mis/users/users.module.ts
+++ src/app/mis/users/users.module.ts
@@ -4,27 +4,21 @@ import { UsersComponent } from './users/users.component';
 import { PortalModule } from 'src/app/portal/portal.module';
 import { CustomElementsModule } from 'src/app/custom-elements/custom-elements.module';
 import { AddUserComponent } from './add-user/add-user.component';
-import { AddUserGroupComponent } from './add-user-group/add-user-group.component';
-import { ViewUserGroupsComponent } from './view-user-groups/view-user-groups.component';
 import { ViewUsersComponent } from './view-users/view-users.component';
 import { UserService } from './user.service';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatTabsModule } from '@angular/material/tabs';
 import { UserDetailsComponent } from './user-details/user-details.component';
-import { GroupDetailsComponent } from './group-details/group-details.component';
 
 
 
 @NgModule({
   declarations: [
     UsersComponent, 
-    AddUserComponent, 
-    AddUserGroupComponent, 
-    ViewUserGroupsComponent, 
+    AddUserComponent,  
     ViewUsersComponent,
-    UserDetailsComponent,
-    GroupDetailsComponent
+    UserDetailsComponent
   ],
   imports: [
     CommonModule,
@@ -39,8 +33,7 @@ import { GroupDetailsComponent } from './group-details/group-details.component';
     UsersComponent
   ],
   entryComponents: [
-    UserDetailsComponent,
-    GroupDetailsComponent
+    UserDetailsComponent
   ],
   providers: [
     UserService
